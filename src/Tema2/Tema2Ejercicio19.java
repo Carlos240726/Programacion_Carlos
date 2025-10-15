59ef06f6076ce76a7c83d79980ad13fd3a5f1849
cargarrom7 <cargarrom7@alu.edu.gva.es>
2025-10-15 14:05:06 +0200

    ASF

---
+++ src/Tema2/Tema2Ejercicio19.java
@@ -0,0 +1,14 @@
+package Tema2;
+
+public class Tema2Ejercicio19 {
+    public static void main(String[] args) {
+
+        int num1 = 0;
+        while (num1 <= 1000) {
+            num1++;
+            if (num1%2==0){
+                System.out.println(num1);
+            }
+        }
+    }
+}
---
+++ src/Tema2/Tema2Ejercicio20.java
@@ -0,0 +1,13 @@
+package Tema2;
+
+import java.util.Scanner;
+
+public class Tema2Ejercicio20 {
+    public static void main(String[] args){
+
+        System.out.println("Dame un numero");
+
+        System.out.println("Dame un numero");
+
+    }
+}
