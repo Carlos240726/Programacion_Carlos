b399baac2eec555b75b8e5400c844c2ae3c2043d
cargarrom7 <cargarrom7@alu.edu.gva.es>
2026-02-04 13:59:50 +0100

    ASF

--- src/Tema5/Banco/Aplicacion.java
+++ src/Tema5/Banco/Aplicacion.java
@@ -1,7 +1,11 @@
 package Tema5.Banco;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aplicacion {
     public static void main(String[] args){
+        List<Usuarios> usuarios=new ArrayList<>();
 
     }
 }
