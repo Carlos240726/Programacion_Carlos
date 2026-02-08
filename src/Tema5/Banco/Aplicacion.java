ff54a6b1d48b316bc90e8d96c0f0c0a78afc433d
Carlos240726 <cargarrom7@alu.edu.gva.es>
2026-02-08 11:42:36 +0100

    Add files via upload

--- src/Tema5/Banco/Aplicacion.java
+++ src/Tema5/Banco/Aplicacion.java
@@ -1,7 +1,67 @@
 package Tema5.Banco;
 
-public class Aplicacion {
-    public static void main(String[] args){
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Aplicacion {
+    public static void menu(){
+        System.out.println("Que quieres hacer?");
+        System.out.println("1. Añadir persona");
+        System.out.println("2. Atender persona");
+        System.out.println("3. Eliminar persona");
+        System.out.println("0. Finalizar bucle");
+    }
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        List<Usuarios> usuarios=new ArrayList<>();
+        boolean continuacion=true;
+        String dni;
+        String nombre;
+        int edad;
+        int posicion=0;
+
+        menu();
+        int opcion = in.nextInt();
+        while (opcion!=0 && opcion!=1 && opcion!=2 && opcion!=3){
+            System.out.println("Opcion no disponible, vuelva a introducir los datos");
+            opcion= in.nextInt();
+        }
+        while (continuacion){
+            switch (opcion){
+                case 0:
+                    continuacion=false;
+                    break;
+                case 1:
+                    System.out.println("Dime el dni");
+                    dni=in.nextLine();
+                    in.nextLine();
+                    System.out.println("Dime el nombre");
+                    nombre= in.nextLine();
+                    in.nextLine();
+                    System.out.println("Dime la edad");
+                    edad=in.nextInt();
+                    usuarios.add(new Usuarios(nombre,dni,edad));
+                    break;
+                case 2:
+                    System.out.println(usuarios.get(0).toString());
+                    System.out.println("Persona atendida");
+                    usuarios.remove(0);
+                    break;
+                case 3:
+                    System.out.println("dame una posicion de la cola, la persona que tenga ese numero sera eliminada");
+                    posicion=in.nextInt();
+                    while (posicion<=0||posicion>usuarios.size()){
+                        System.out.println("Posicion no valida, intentelo de nuevo");
+                        posicion= in.nextInt();
+                    }
+                    posicion=posicion-1;
+                    System.out.println(usuarios.get(posicion).toString());
+                    System.out.println("persona eliminada");
+                    usuarios.remove(posicion);
+                    break;
+            }
+        }
     }
 }
---
+++ src/Tema5/Compra/CompraEjercicio1.java
@@ -0,0 +1,39 @@
+package Tema5.Compra;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class CompraEjercicio1 {
+    public static void main(String[] args){
+    LinkedHashSet <String> listaCompra = new LinkedHashSet<>();
+    Scanner in = new Scanner(System.in);
+
+    boolean verificacion=true;
+    int opcion=0;
+    String valor= "";
+
+    while (verificacion){
+        System.out.println("Quieres añadir algo a la lista (0) o simplemente finalizar el proceso? (1)");
+        opcion=in.nextInt();
+        while (opcion!=1 && opcion!=0 ){
+            System.out.println("Valor no valido, vuelve a intentarlo");
+            opcion=in.nextInt();
+            }
+        switch (opcion){
+            case 0:
+                System.out.println("Introduce lo que quieres añadr a la lista");
+                valor=in.next();
+                if (listaCompra.contains(valor)){
+                    System.out.println("El producto ya existe en la lista");
+                }
+                listaCompra.add(valor);
+                break;
+            case 1:
+                System.out.println("Finalizando proceso...");
+                verificacion=false;
+                break;
+            }
+        }
+        System.out.println(listaCompra);
+    }
+}
